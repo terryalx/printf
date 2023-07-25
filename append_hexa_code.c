3bc7e40950eaa715a7acea8b07cf544abb0135a1
setalxswe <setalxswe@gmail.com>
2023-07-25 14:38:19 -0700

    assci_code_fmt

--- append_hexa_code.c
+++ append_hexa_code.c
@@ -12,14 +12,13 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
 
-	if (ascii_code < 0)
-		ascii_code *= -1;
+	unsigned char code = (unsigned char)ascii_code;
 
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
 
-	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
+	buffer[i++] = map_to[code / 16];
+	buffer[i] = map_to[code % 16];
 
 	return (3);
 }
