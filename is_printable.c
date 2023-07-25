4cbc47cdb36763a69949d4b34e6230b5154742c3
setalxswe <setalxswe@gmail.com>
2023-07-25 13:11:20 -0700

    test0x02

--- is_printable.c
+++ is_printable.c
@@ -3,9 +3,16 @@
 /**
  * is_printable - check if a c is printable
  * @c: Character to check
+ * Return: 1 -> printable, else 0
  */
 
 int is_printable(char c)
 {
+	int i = 0;
+	int n = 1;
+
 	if (c >= 32 && c < 127)
+		return (n);
+
+	return (i);
 }
