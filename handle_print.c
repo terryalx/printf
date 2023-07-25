ab70e3554226a352d84387dbe64aafd947eb6639
setalxswe <setalxswe@gmail.com>
2023-07-25 02:13:43 -0700

    bettytest0x0

--- handle_print.c
+++ handle_print.c
@@ -14,7 +14,7 @@
  */
 
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-	int flags, int width, int pr, int size)
+		int flags, int width, int pr, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
